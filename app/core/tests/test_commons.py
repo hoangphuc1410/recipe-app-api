dc6621efcd84a4c69d0cd45c8014d9a4c10e0a92
Phuc <hoangphuc2984@gmail.com>
2025-02-17 21:16:32 +0700

    Fix name import

--- app/core/tests/test_commons.py
+++ app/core/tests/test_commons.py
@@ -2,7 +2,7 @@
 Test custom Django management commands.
 """
 from unittest.mock import patch
-from pyscopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
